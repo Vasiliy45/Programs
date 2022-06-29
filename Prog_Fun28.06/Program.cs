1d5c96ab3209fe60b8dd009f9037cb6288ba9e39
vasiliy <abramovvp45@gmail.com>
2022-06-29 13:20:18 +0500

    функция

---
+++ Prog_Fun28.06/Program.cs
@@ -0,0 +1,22 @@
+﻿ int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+if (arg2 > result) result = arg2;
+if (arg3 > result) result = arg3;
+return result;
+}
+
+
+int a1=15;
+int a2=17;
+int a3=14;
+int b1=1008;
+int b2=20;
+int b3=22;
+int c1=28;
+int c2=300;
+int c3=29;
+
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.WriteLine( max + " большее число");
