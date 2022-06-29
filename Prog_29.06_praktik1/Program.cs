0f29fff212001c9f4e339a00d706695e7eb2b38b
vasiliy <abramovvp45@gmail.com>
2022-06-29 19:24:24 +0500

    практика 1 часть

---
+++ Prog_29.06_praktik1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Console.WriteLine ("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int summ = numberA*numberA;
+Console.WriteLine(summ);*/
+
+/*Console.WriteLine ("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB*numberB)
+{
+  Console.WriteLine ("Является квадатом числа");
+
+} else 
+{
+    Console.WriteLine ("Не является квадратом");
+}*/
+
+Console.WriteLine ("Введите номер дня");
+int A = Convert.ToInt32(Console.ReadLine());
+if (A == 1) {
+  Console.WriteLine ("понедельник");  
+}
+if (A == 2) {
+  Console.WriteLine ("вторник");  
+}
+if (A == 3) {
+  Console.WriteLine ("среда");  
+}
+if (A == 4) {
+  Console.WriteLine ("четверг");  
+}
+if (A == 5) {
+  Console.WriteLine ("пятница");  
+}
+if (A == 6) {
+  Console.WriteLine ("суббота");  
+}
+if (A == 7) {
+  Console.WriteLine ("воскресенье");  
+} else {Console.WriteLine ("ошибка");}
