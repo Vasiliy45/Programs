54b21fd4dfe850cd19b6c29381cdc23993e8978b
vasiliy <abramovvp45@gmail.com>
2022-06-29 15:00:32 +0500

    massiv1

---
+++ Prog_29.06mass/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] mass = {1, 2, 3, 4, 8, 5, 6, 4, 8, 9};
+int n = mass.Length;
+int find = 4;
+int index = 0;
+while(index<n)
+{
+    if (mass [index] ==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
