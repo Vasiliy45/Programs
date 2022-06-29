2440b814e92900bc0b0d214a2cb0b74b93b0f6bf
vasiliy <abramovvp45@gmail.com>
2022-06-29 23:40:56 +0500

    Домашняя работа 2_13

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine ("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n<=99) {
+  Console.WriteLine ("третьей цифры нет");  
+}
+   else  {   Console.WriteLine("Третье число " + n.ToString()[2] );
+   }
