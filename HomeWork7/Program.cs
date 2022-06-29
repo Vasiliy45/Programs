dd50de276e5a6317f1ff43a6957c246dafce12bc
vasiliy <abramovvp45@gmail.com>
2022-06-30 00:01:58 +0500

    Домашняя работа 2_15

---
+++ HomeWork7/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите номер дня недели");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day<1 || day >7) 
+{
+Console.WriteLine ("таких дней нет");
+} else if (day == 6 || day==7 ) {
+   Console.WriteLine ("день выходной"); 
+} else {
+    Console.WriteLine ("работать негры");
+}
