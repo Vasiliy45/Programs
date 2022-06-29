34125b8a55d108d39e877cb7c773197efdd24111
vasiliy <abramovvp45@gmail.com>
2022-06-29 20:07:23 +0500

    Домашняя работа 1_4

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число число");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите третье число число");
+int C = Convert.ToInt32(Console.ReadLine());
+int max = A;
+if (B > max) max=B ;
+if (C > max) max=C;
+Console.WriteLine ("максимальное " + max);
