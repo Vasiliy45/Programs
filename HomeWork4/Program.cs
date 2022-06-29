51f3e38c4af4cc04d7c0d23731d22de8e3b2016a
vasiliy <abramovvp45@gmail.com>
2022-06-29 21:11:11 +0500

    Домашняя работа 1_8

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= A)
+{
+    if (i % 2 == 0) 
+    {
+     Console.Write (i + ", ");
+    }
+      i++;
+}
