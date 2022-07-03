bd395f82befbbcd352435f6110a37732ab544d0b
vasiliy <abramovvp45@gmail.com>
2022-07-03 21:27:18 +0500

    Домашняя работа 3_23

---
+++ HomeWork8/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] calkule (int num)
+{
+  int[] A =new int [num];
+  int i=1;
+  while (i <= num) {
+    A [i-1]=i*i*i;
+    Console.WriteLine (A[i-1]);
+    i++;}
+    return A;
+   
+}
+int [] resault = calkule(3);
