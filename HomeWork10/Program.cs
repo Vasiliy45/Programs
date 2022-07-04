370944c5d556f4576183f5528f9cf4388f1be7c5
vasiliy <abramovvp45@gmail.com>
2022-07-04 13:10:55 +0500

    Домашняя работа 3_19

---
+++ HomeWork10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("Введите пятизначное число");
+
+int N = Convert.ToInt32(Console.ReadLine());
+if ( N<10000 || N>100000) 
+{
+ Console.WriteLine ("Число введено не правильно");   
+} else {
+int a = (N%10)/1;
+int c = (N%100)/10;
+//int a = (N%1000)/100;
+int d = (N%10000)/1000;
+int b = (N%100000)/10000;
+if (a==b && c==d){
+    Console.WriteLine ("Число палиндром");
+} else Console.WriteLine ("Число не палиндром");
+}
