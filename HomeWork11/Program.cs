7c76af0a1caf41f66f84af16782f2959bd9fe51f
vasiliy <abramovvp45@gmail.com>
2022-07-08 20:16:37 +0500

    Домашняя работа 4_29

---
+++ HomeWork11/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] arr(int N)
+{
+int [] resalt = new int [N];
+for(int i=0; i<N; i++)
+{
+    int A= (new Random()).Next(0,10); 
+    resalt[i]= A;
+   Console.Write(resalt[i]+" ");
+}
+return resalt;
+}
+arr (8);
