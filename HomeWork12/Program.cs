dc3324abdb12c04d1b3702f0f7f0bf963d6ecd98
vasiliy <abramovvp45@gmail.com>
2022-07-08 21:01:56 +0500

    Домашняя работа 4_25

---
+++ HomeWork12/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите  число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите  число B");
+int b = Convert.ToInt32(Console.ReadLine());
+double Step( int a, int b)
+{
+    double result = Math.Pow(a, b);
+    
+    Console.WriteLine ($" число  А  {a} в степени B {b} = {result}");
+   
+    return result;
+    
+}
+Step (a,b);
